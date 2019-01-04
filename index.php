a1d4f795df41a3f0306516b859400ccfbc14bc61
demidovitch2 <substancia.lilmoses@gmail.com>
2019-01-04 19:12:11 +0200

    Template do Admin

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once("vendor/autoload.php");
 use \util\Page;
+use \util\PageAdmin;
 
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = true;
@@ -13,6 +14,14 @@ $app = new \Slim\App(['settings' => $config]);
 $app->get('/', function() {
     
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
